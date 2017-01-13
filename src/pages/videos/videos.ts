d7c06cee6c1ed00a716567654e0a577c213045e9
Juan G. Jordán <juangui@gmail.com>
2017-01-13 01:38:51 +0100

    Icon and splash

--- src/pages/videos/videos.ts
+++ src/pages/videos/videos.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
-import { File, VideoPlayer } from 'ionic-native';
 
 @Component({
   selector: 'page-videos',
@@ -14,29 +13,29 @@ export class VideosPage {
 
   }
 
-  play(fileName) {
-    let filePath: string;
+  // play(fileName) {
+  //   let filePath: string;
 
-    this.platform.ready().then(() => {
-      if (this.platform.is('cordova')) {
-        filePath = 'file:///android_asset/www/video/' + fileName + '.mp4';
-      } else {
-        filePath = './assets/doc/' + fileName + '.mp4';
-      }
-      console.log('Open video at ' + filePath);
+  //   this.platform.ready().then(() => {
+  //     if (this.platform.is('cordova')) {
+  //       filePath = 'file:///android_asset/www/video/' + fileName + '.mp4';
+  //     } else {
+  //       filePath = './assets/doc/' + fileName + '.mp4';
+  //     }
+  //     console.log('Open video at ' + filePath);
 
-      if (this.platform.is('cordova')) {
-        // Playing a video.
-        VideoPlayer.play(filePath).then(() => {
-          console.log('video completed');
-        }).catch(err => {
-          console.log(err);
-        });
-      } else {
-        window.open(filePath, '_blank');
-      }
-    });
+  //     if (this.platform.is('cordova')) {
+  //       // Playing a video.
+  //       VideoPlayer.play(filePath).then(() => {
+  //         console.log('video completed');
+  //       }).catch(err => {
+  //         console.log(err);
+  //       });
+  //     } else {
+  //       window.open(filePath, '_blank');
+  //     }
+  //   });
 
-  }
+  // }
 
 }
